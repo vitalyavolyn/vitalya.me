6a840167c264adae666d74dfae92eaae81e60079
Vitaliy Volynskiy <i@vitalya.me>
2025-11-20 17:05:27 +0500

    feat: cache map image

--- src/utils/devon.ts
+++ src/utils/devon.ts
@@ -6,7 +6,19 @@ export async function getToday() {
   return data;
 }
 
-// TODO: cache image??
+let cachedCoords = { lat: 0, lon: 0 };
+let cachedMap = "";
 export async function getMapUrl(wttr: any) {
-  return `https://api.mapbox.com/styles/v1/mapbox/dark-v11/static/${wttr.areaLongitude},${wttr.areaLatitude - 0.05},10,0/1280x380@2x?access_token=${import.meta.env.MAPBOX_KEY}`;
+  const lon = wttr.areaLongitude;
+  const lat = wttr.areaLatitude - 0.05;
+  if (cachedMap && cachedCoords.lat === lat && cachedCoords.lon === lon) {
+    return cachedMap;
+  }
+
+  const url = `https://api.mapbox.com/styles/v1/mapbox/dark-v11/static/${lon},${lat},10,0/1280x380@2x?access_token=${import.meta.env.MAPBOX_KEY}`;
+  const response = await fetch(url);
+  const buffer = await response.arrayBuffer();
+  cachedMap = `data:image/png;base64,${Buffer.from(buffer).toString("base64")}`;
+  cachedCoords = { lat, lon };
+  return cachedMap;
 }
